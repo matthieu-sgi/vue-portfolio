bdd79c04b51b8b8546084a4ca8aae7e00879d4ce
sn00wden <matthieu2603@gmail.com>
2023-10-06 13:47:54 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <MyNavbar/>
+    <MyHomepage/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import MyHomepage from './components/MyHomepage.vue'
+import MyNavbar from './components/MyNavbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyHomepage,
+    MyNavbar
   }
 }
 </script>
@@ -23,6 +27,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,40 @@
-import Vue from 'vue'
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import {Vue, createApp} from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
+import MyProjects from './components/MyProjects.vue'
+import VueRouter from 'vue-router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+const routes = [
+  { path: '/', component: App },
+  {path: '/projects', component: MyProjects},
+];
+
+const router = new VueRouter({
+  routes: routes, // short for `routes: routes`
+  mode: 'history',
+});
+
+// router.push('/projects');
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+  
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// const app = new Vue({
+//   render: h => h(App),
+// });
+const app = Vue.createApp({});
+app.use(router);
+app.mount('#app')
